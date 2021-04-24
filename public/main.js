f69ea9b669e7286a76791e5d9610889a9c971bed
Lucas Morais <lucas.morais446@hotmail.com>
2021-04-24 12:15:44 -0300

    Colors - #4

--- public/main.js
+++ public/main.js
@@ -25,7 +25,7 @@ if (!gl) {
 
 // === END OF PIPE === //
 
-// 1TH
+// ========================== 1TH ========================== //
 const vertexData = [
     -1, -1, 0, //V0.position
     0, 1, 0,   //V1.position
@@ -38,16 +38,25 @@ const colorData = [
     0, 0, 1,   //V2.color
 ];
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
-// 2TH
+// ========================== 2TH ========================== //
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
+precision mediump float;
+
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
 void main() {
+    vColor = color;
     gl_Position = vec4(position, 1);
 }
 `);
@@ -55,9 +64,12 @@ gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
+precision mediump float;
+
+varying vec3 vColor;
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    gl_FragColor = vec4(vColor, 1.0);
 }
 `);
 gl.compileShader(fragmentShader);
@@ -68,10 +80,16 @@ gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 
 
-// 3TH
+// ========================== 3TH ========================== //
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
